4e35bc0f07fb022ed857772aeb89d90569051d3b
Ordo Adrian <ordoadrian@Ordos-MacBook-Pro.local>
2023-01-16 11:36:10 +0000

    I have added a finalScoreUpdate() that saves score to memeory and updates it, also added event listeners to all answer buttons that check if it was clicked and they do a cool thing of checking if the answer clicked is correct, if it is then score gets updated, if not then time goes down by 10 sec (or whatever you set it to really)

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -11,6 +11,8 @@ startBtn.addEventListener("click", function() {
     setTime();
 });
 
+// !Questions section
+
 // Create ordered list items
 let listEl = document.createElement("ol");
 let answer1li = document.createElement("li");
@@ -29,7 +31,11 @@ answer1btn.textContent = questionsArr[0].choices[0];
 answer2btn.textContent = questionsArr[0].choices[1];
 answer3btn.textContent = questionsArr[0].choices[2];
 answer4btn.textContent = questionsArr[0].choices[3];
-
+// Adds class to the answers
+answer1btn.setAttribute("class", "answer1btn")
+answer2btn.setAttribute("class", "answer2btn")
+answer3btn.setAttribute("class", "answer3btn")
+answer4btn.setAttribute("class", "answer4btn")
 
 // this adds the list to choices div within the DOM
 choicesDiv.appendChild(listEl)
@@ -59,10 +65,79 @@ answer4btn.addEventListener("click", function() {
     console.log(`Im answer4 and I have been clicked!`)
 });
 
-// This is the timer section
+// TODO: need to create a function that replaces the #questions-title title
+// TODO: with next question and the same with the questions them selves.
+// TODO: Also this need to have tracking for and count score every time you get the answer
+// TODO: right and it needs to deduct time for every wrong answer.
+
+let questNumTrack = 1
+let score = localStorage.getItem("score");
+let finalScoreEl = document.querySelector("#final-score")
+
+let nextQuest = function() {
+    // Changes the h2 to the new question
+    questionTitleh2.textContent = questionsArr[questNumTrack].question;
+    // This fills list with new questions
+    answer1btn.textContent = questionsArr[questNumTrack].choices[0];
+    answer2btn.textContent = questionsArr[questNumTrack].choices[1];
+    answer3btn.textContent = questionsArr[questNumTrack].choices[2];
+    answer4btn.textContent = questionsArr[questNumTrack].choices[3];
+
+    questNumTrack++
+};
+
+// Function that creates, updates the score. stored locally in users browser.
+let finalScoreUpdate = function() {
+    finalScoreEl.textContent = score;
+    score++
+    localStorage.setItem("score", score);
+}
+console.log(`This is the questionsArr[0].correctAnswer: ${questionsArr[0].correctAnswer}`)
+console.log(`This is the answer1btn.textContent: ${answer1btn.textContent}`)
+// Event listener that looks at the answers and adds to the score or lowers time.
+answer1btn.addEventListener("click", function() {
+    if (answer1btn.textContent == questionsArr[0].correctAnswer) {
+        finalScoreUpdate();
+    } else {
+        secondsLeft = secondsLeft - 10;
+    };
+    nextQuest();
+});
+answer2btn.addEventListener("click", function() {
+    if (answer1btn.textContent == questionsArr[1].correctAnswer) {
+        finalScoreUpdate();
+    } else {
+        secondsLeft = secondsLeft - 10;
+    };
+    nextQuest();
+});
+answer3btn.addEventListener("click", function() {
+    if (answer1btn.textContent == questionsArr[2].correctAnswer) {
+        finalScoreUpdate();
+    } else {
+        secondsLeft = secondsLeft - 10;
+    };
+    nextQuest();
+});
+answer4btn.addEventListener("click", function() {
+    if (answer1btn.textContent == questionsArr[3].correctAnswer) {
+        finalScoreUpdate();
+    } else {
+        secondsLeft = secondsLeft - 10;
+    };
+    nextQuest();
+});
+
+
+
+
+// !End of Questions section
+
+
+// !Timer section
 
 let timeEl = document.querySelector("#time");
-var secondsLeft = 60;
+let secondsLeft = 60;
 
 function setTime() {
   // Sets interval in variable
@@ -73,24 +148,25 @@ function setTime() {
     if(secondsLeft === 0) {
       // Stops execution of action at set interval
       clearInterval(timerInterval);
-      finalScore();
+    //   finalScore();
     }
 
   }, 1000);
 }
 
+// !End of timer section
 
-// End screen section
+// !End-screen section
 
-// This is not needed, the final score will be the number of questions answered.
-let finalScoreEl = document.querySelector("#final-score")
+// // This is not needed, the final score will be the number of questions answered.
+// let finalScoreEl = document.querySelector("#final-score")
 
-let finalScore = function() {
-    finalScoreEl.textContent = secondsLeft
-};
+// let finalScore = function() {
+//     finalScoreEl.textContent = secondsLeft
+// };
 
-// Function makes sure all other sections of quiz game are hidden
-// and displays the End score screen.
+// !Function makes sure all other sections of quiz game are hidden
+// !and displays the End score screen.
 let endScreenEl = document.querySelector("#end-screen")
 let dispEndScreen = function() {
     if (questionsSectDiv.className == "show") {
@@ -102,5 +178,6 @@ let dispEndScreen = function() {
     endScreenEl.setAttribute("class", "show");
 }
 
+// !End of end-screen section
 
 // TODO: Write a function that tracks number of questions answered.
