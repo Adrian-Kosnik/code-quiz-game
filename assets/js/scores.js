8ead3fa88eab4f049812fa64039cb2be65c552bd
Ordo Adrian <ordoadrian@Ordos-MacBook-Pro.local>
2023-02-14 13:43:44 +0000

    added comments

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,12 +1,14 @@
-// This is the highscres <ol> element
+//! Selecting elements section
 let scoresEl = document.querySelector("#scores");
 let clearScoresBtn = document.querySelector("#clear");
+//! End of selecting elements section
 
+//! Getting elements from localStorage
 let something = localStorage.getItem('results');
-
 let userScoresStored = JSON.parse(something);
-console.log(userScoresStored);
+//! End of getting elements from localStorage
 
+// For loop that creates li elements for every score in localStorage
 for (let i = userScoresStored.length - 1; i >= 0; i--) {
     let listEl = document.createElement("li");
     listEl.setAttribute("class", "scoreLi");
@@ -15,6 +17,8 @@ for (let i = userScoresStored.length - 1; i >= 0; i--) {
     scoresEl.appendChild(listEl);
 };
 
+// Event listener on the 'Clear Scores' button that removes the scores from
+// localStorage and also removes the <li> elements that have scores.
 clearScoresBtn.addEventListener("click", function() {
     localStorage.clear();
     scoresEl.remove();
